6527492990e560d4461d01d9f0d0382596c04f1c
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-07-25 23:06:48 +0200

    Now it's a web app

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.format : format;
+import vibe.vibe;
 
 /// A Greeter class
 class Greeter
@@ -29,4 +30,10 @@ void main()
 	writeln(m);
 
 	writeln("That was the greeting!");
+
+	listenHTTP("127.0.0.1:8080", (req, res) {
+        res.writeBody("Hello Vibe.d: " ~ req.path);
+    });
+
+    runApplication();
 }
