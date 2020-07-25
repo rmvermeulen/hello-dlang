c3a4c5864736eb77dc771d544f7c55a98d0f8569
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-07-25 23:17:38 +0200

    Example web app

--- source/app.d
+++ source/app.d
@@ -1,39 +1,19 @@
-import std.stdio;
-import std.format : format;
 import vibe.vibe;
 
-/// A Greeter class
-class Greeter
+void index(HTTPServerRequest req, HTTPServerResponse res)
 {
-	public int getN()
-	{
-		return 5;
-	}
-
-	string getMessage()
-	{
-		return "This is the message!";
-	}
+	res.render!("index.dt", req);
 }
 
 void main()
 {
-
-
-	auto g = new Greeter();
-	writeln(format!"Created a greeter! %s"(g));
-
-	const int n = g.getN();
-	writeln(format!"Got a number: %s"(n));
-
-	auto m = g.getMessage();
-	writeln(m);
-
-	writeln("That was the greeting!");
-
-	listenHTTP("127.0.0.1:8080", (req, res) {
-        res.writeBody("Hello Vibe.d: " ~ req.path);
-    });
-
-    runApplication();
+	auto router = new URLRouter;
+	router.get("/", &index);
+	
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	
+	listenHTTP(settings, router);
+	
+	runApplication();
 }
