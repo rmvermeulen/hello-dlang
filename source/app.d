33b2d000b042df5e96f26513636c6aa6c8a33f01
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-07-25 15:10:43 +0200

    More buggy code

--- source/app.d
+++ source/app.d
@@ -1,16 +1,91 @@
 import std.stdio;
-// import vibe.vibe;
+import std.format : format;
 
-class Greeter {
-	auto getMessage() {
+/// A statically sized vec<T>
+template Vector(T, int N)
+{
+}
+
+template Vector(T, int N : 1)
+{
+	private T[N] values;
+	T x()
+	{
+		return values[0];
+	}
+}
+
+template Vector(T, int N : 2)
+{
+	private T[N] values;
+	T x()
+	{
+		return values[0];
+	}
+
+	T y()
+	{
+		return values[1];
+	}
+}
+
+template Vector(T, int N : 3)
+{
+	private T[N] values;
+	T x()
+	{
+		return values[0];
+	}
+
+	T y()
+	{
+		return values[1];
+	}
+
+	T z()
+	{
+		return values[2];
+	}
+}
+
+/// A Greeter class
+class Greeter
+{
+	public int getN()
+	{
+		return 5;
+	}
+
+	string getMessage()
+	{
 		return "This is the message!";
 	}
 }
 
-
 void main()
 {
-	Greeter g;
+	auto v1 = new Vector!(int, 1);
+	writeln(v1.x());
+
+	auto v2 = new Vector!(int, 2);
+	writeln(v2.x());
+	writeln(v2.y());
+
+	auto v3 = new Vector!(int, 3);
+	writeln(v3.x());
+	writeln(v3.y());
+	writeln(v3.z());
+
+	writeln(format!"Created a Vector! %s"(*vec));
+
+	auto g = new Greeter();
+	writeln(format!"Created a greeter! %s"(g));
+
+	const int n = g.getN();
+	writeln(format!"Got a number: %s"(n));
+
 	auto m = g.getMessage();
 	writeln(m);
+
+	writeln("That was the greeting!");
 }
