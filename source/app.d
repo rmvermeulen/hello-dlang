fe99d64a8ffd47ab5dd32b2bdb771f40bdb258f7
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-07-26 11:31:39 +0200

    tests pass/run on linux

--- source/app.d
+++ source/app.d
@@ -5,20 +5,23 @@ void index(HTTPServerRequest req, HTTPServerResponse res)
 	res.render!("index.dt", req);
 }
 
-version(unittest) {
+version (unittest)
+{
 	// Do nothing here, dub takes care of that
-} else {
+}
+else
+{
+	// run the app
 	void main()
 	{
 		auto router = new URLRouter;
 		router.get("/", &index);
-		
+
 		auto settings = new HTTPServerSettings;
 		settings.port = 8080;
-		
+
 		listenHTTP(settings, router);
-		
+
 		runApplication();
 	}
 }
-
